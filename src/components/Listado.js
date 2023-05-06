eefcb4e0e79a33d2b176bd99e6dd67b580b2865f
[mciannello] <[mciannello@gmail.com]>
2023-05-06 18:42:10 -0300

    Agragdo grabado de token

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,6 +1,15 @@
-import React from "react";
+// import { useEffect } from "react";
+// import {useHistory} from 'react-router-dom';
 
 function Listado(){
+    // const history=useHistory();
+
+    // useEffect(()=>{
+    //     const token=localStorage.getItem('token');
+    //     if (token===null){
+    //         history.push('/');
+    //     }
+    // }, [history]);
 return(
     <h2>Soy el componente listado</h2>
 )
