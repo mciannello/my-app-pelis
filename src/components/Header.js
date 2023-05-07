3522a7433d5695314ac0c7d63decafb5993be9d6
[mciannello] <[mciannello@gmail.com]>
2023-05-07 17:12:47 -0300

    Busqueda+API

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,27 @@
 import { Link } from "react-router-dom";
+import Busqueda from "./Busqueda";
 
 function Header(){
     return(
-        <header className="p-3 text-bg-dark">
-            <nav className="navbar navbar-expand-lg" data-bs-theme="dark">
-                <h2 className="navbar-brand">MCI</h2>
-                <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                    <li className="nav-item dark" >
-                        <Link className="nav-link px-2 text-secondary" to="/">Home</Link>
-                    </li>
-                    <li className="nav-item dark">
-                    <Link className="nav-link px-2 text-white" to="/listado">Listado</Link>
-                    </li>
-                    <li className="nav-item dark">
-                    <Link className="nav-link px-2 text-white" to="/contacto">Contacto</Link>
-                    </li>
-                </ul>
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark" data-bs-theme="dark">
+                <div className="container">
+                    <Link to="/" className="navbar-brand">MCI</Link>
+                    <div className="collpase navbar-collapse" id="navbarNav">
+                        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                        <li className="nav-item dark" >
+                            <Link className="nav-link px-2 text-secondary" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item dark">
+                        <Link className="nav-link px-2 text-white" to="/listado">Listado</Link>
+                        </li>
+                        <li className="nav-item dark">
+                        <Link className="nav-link px-2 text-white" to="/contacto">Contacto</Link>
+                        </li>
+                        </ul>
+                    </div>
+                    <Busqueda/>
+                </div>
             </nav>
         </header>
     )
